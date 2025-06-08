36f9387539935e299a21a3b1fdf9f45746f24535
hyesung <hwanghs7008@g.skku.edu>
2025-06-08 17:12:19 +0900

    [Feat] Service 리팩토링

--- src/main/java/org/omok/newomok/service/CheckIdService.java
+++ src/main/java/org/omok/newomok/service/CheckIdService.java
@@ -1,7 +1,22 @@
 package org.omok.newomok.service;
 
 import lombok.extern.log4j.Log4j2;
+import org.omok.newomok.domain.UserVO;
+import org.omok.newomok.repository.userDAO;
 
 @Log4j2
 public enum CheckIdService {
+    INSTANCE;
+    private userDAO dao;
+
+    CheckIdService(){
+        dao = userDAO.INSTANCE;
+    }
+
+
+    //아이디 중복 체크를 위함
+    public boolean isExistId(String userId){
+        return dao.isExistUserById(userId);
+    }
+
 }
--- src/main/java/org/omok/newomok/service/LogoutService.java
+++
@@ -1,7 +0,0 @@
-package org.omok.newomok.service;
-
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
-public enum LogoutService {
-}
